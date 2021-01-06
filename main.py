bc0a78e7a8d1059531954ee31b10c4eef8de6da3
Royce Chua <royce236@gmail.com>
2021-01-06 20:35:55 +0800

    Implement SQLite Setup in main.py

--- main.py
+++ main.py
@@ -1,44 +1,47 @@
-from typing import Optional
+from typing import List
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import Depends, FastAPI, HTTPException
+
+from sqlalchemy.orm import Session
+
+from db import crud, models, schemas, database
+from db.database import SessionLocal, engine
 
 todo_app_api = FastAPI()
 
-todo_list = [
-    {
-        "id": 1,
-        "task": "Do the dishes",
-        "isDone": False
-    }
-]
+# todo_list = [
+#     {
+#         "id": 1,
+#         "task": "Do the dishes",
+#         "isDone": False
+#     }
+# ]
 
-class todo(BaseModel):
-    id: int
-    task: str
-    isDone: Optional[bool] = False
+models.Base.metadata.create_all(bind=engine)
 
-@todo_app_api.get("/todos")
-async def return_all_todos():
-    return {"todo_list": todo_list}    
-
-@todo_app_api.get("/todo/{id}")
-async def return_specific_todo(id: int):
+# Dependency
+def get_db():
+    db = SessionLocal()
     try:
-        for existing_todo in todo_list:
-            if(existing_todo["id"] == id):
-                return existing_todo
-    except Exception as err:
-            return {"message": f"{err}"}
-    return {"message": "No such todo exists"} 
+        yield db
+    finally:
+        db.close()
+
+@todo_app_api.get("/todos", response_model=List[schemas.TodoBase])
+async def return_all_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    todos = crud.get_todos(db, skip=skip, limit=limit)
+    return todos  
+
+@todo_app_api.get("/todo/{id}", response_model=schemas.TodoBase)
+async def return_specific_todo(id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id=id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
 
 @todo_app_api.post("/todo/add")
-async def add_todo(todo: todo):
-    try:
-        todo_list.append(dict(todo))
-    except Exception as err:
-        return {"message": f"{err}"}
-    return {"message": "Todo added"} 
+async def add_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    return crud.create_todo(db=db, todo=todo)
 
 @todo_app_api.delete("/todo/{id}")
 async def delete_todo(id: int):
@@ -51,7 +54,7 @@ async def delete_todo(id: int):
     return {"message": f"Todo #{id} Deleted"}  
  
 @todo_app_api.put("/todo/{id}")
-async def update_todo(id: int, updated_todo: todo):
+async def update_todo(id: int, updated_todo: schemas.TodoBase):
     try:
         for existing_todo in todo_list:
             if(existing_todo["id"] == id):
